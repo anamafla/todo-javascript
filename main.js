39d40e07cbbc8cd83b5bfd86f835eb99e83e7b70
Ana Mafla <anamafla@hotmail.com>
2019-08-04 19:33:18 -0500

    Add filters

--- main.js
+++ main.js
@@ -1,46 +1,102 @@
-let todos = [
-  {
-    task: "Make a list",
-    done: false
-  }
-];
+let todos = [];
+
+let filter = false;
+
+let pendingTasks = 0;
 
 render();
 
 function render() {
   const app = document.querySelector("#app");
 
+  pendingTasks = todos.reduce(
+    (total, { done }) => (!done ? total + 1 : total),
+    0
+  );
+
+  const doneTasks = todos.length - pendingTasks;
+
   app.innerHTML = `
   <div class="card-body">
   <header class="form-group">
-  <label>New Task</label>
+  <label>New Task</label><button class="set-all btn btn-sm float-right">${
+    pendingTasks === 0 ? "Restaure" : "Complete"
+  } all </button>
   <input class="form-control w-100" placeholder="Enter a new task" />
   </header>
   <section>
   <ul class="list list-group mt-4"></ul>
   </section>
-  <footer> </footer>
+  <footer class="mt-4 justify-content-between align-items-center ${
+    todos.length > 0 ? "d-flex" : "d-none"
+  }">
+  <small>${pendingTasks} pending task${pendingTasks === 1 ? "" : "s"}</small>
+  <div class="filters">
+   <button class="btn btn-sm"> All </button>
+   <button class="btn btn-sm"> Pending </button>
+   <button class="btn btn-sm"> Completed </button>
+  </div>
+  <button class="clear-all btn btn-sm">Clear complete </button>
+  
+  </footer>
   </div>
   `;
 
-  let list = document.querySelector(".list-group");
-  list.append(...todos.map(createTask));
-
   let input = document.querySelector("input");
   input.addEventListener("keypress", handleInput);
+
+  //Setting Filters
+  setFilters(...app.querySelectorAll(".filters .btn"));
+
+  //Filter List
+  let todosFiltered = todos.filter(({ done }) => {
+    switch (filter) {
+      case "completed":
+        return done;
+      case "pending":
+        return !done;
+      default:
+        return true;
+    }
+  });
+
+  let list = document.querySelector(".list-group");
+  list.append(...todosFiltered.map(createTask));
+
+  //Complete All
+  app.querySelector(".set-all").addEventListener("click", completeAll);
+
+  //Clear all
+  app.querySelector(".clear-all").addEventListener("click", clearAll);
 
   console.log(todos);
 }
 
 function handleInput(e) {
   if (e.keyCode === 13 && e.target.value) {
-    console.log(e.target.value);
     todos.push({
       task: e.target.value,
       done: false
     });
     render();
   }
+}
+
+function setFilters(all, pending, completed) {
+  all.addEventListener("click", () => {
+    filter = false;
+    render();
+  });
+
+  pending.addEventListener("click", () => {
+    filter = "pending";
+    render();
+  });
+
+  completed.addEventListener("click", () => {
+    filter = "completed";
+    render();
+  });
 }
 
 function createTask(element, index) {
@@ -68,4 +124,21 @@ function createTask(element, index) {
   });
 
   return item;
+}
+
+function completeAll() {
+  todos.forEach(todo => {
+    if (pendingTasks === 0) {
+      todo.done = false;
+    } else {
+      todo.done = true;
+    }
+  });
+  render();
+}
+
+function clearAll() {
+  todos.forEach(todo => (todo.done = false));
+
+  render();
 }
